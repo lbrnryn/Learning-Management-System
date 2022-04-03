78a185fe83ccbd2bced039864a9a689008f44279
Ryan <lbrnryn@yahoo.com>
2022-04-04 00:45:20 +0800

    Update: DB link

--- db.js
+++ db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 module.exports = async function db() {
   try {
-    await mongoose.connect(process.env.NODE_ENV == 'development' ? 'mongodb://127.0.0.1:27017/icctlms' : process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI); //process.env.NODE_ENV == 'development' ? 'mongodb://127.0.0.1:27017/icctlms' : process.env.MONGO_URI
     console.log('Database connected!');
+    // console.log(process.env.MONGO_URI)
   } catch (err) {
     console.log(err.message);
   }
